6e843c69556b662c7a08ab32995af4e53fa48f8e
Marshall Moats <marshallmoats@gmail.com>
2024-08-09 21:20:58 -0500

    Fri Aug  9 09:20:58 PM CDT 2024

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
