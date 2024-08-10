44e0b05dbde0ad4cdf5630f5ee73bd06497ec887
Marshall Moats <marshallmoats@gmail.com>
2024-08-09 21:06:20 -0500

    Fri Aug  9 09:06:20 PM CDT 2024

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-use std::net::{TcpListener, TcpStream};
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
-fn handle_client(stream: TcpStream) {
-    println!("Received connection from {stream:?}");
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
 }
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:9999")?;
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
 
-    // accept connections and process them serially
-    for stream in listener.incoming() {
-        handle_client(stream?);
-    }
-    Ok(())
+    println!("Request: {http_request:#?}");
 }
