49c7f3b13d88ba0a208cb6fdb168781319c70be6
Marshall Moats <marshallmoats@gmail.com>
2024-08-09 21:40:40 -0500

    Fri Aug  9 09:40:40 PM CDT 2024

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,24 @@ use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
+use std::{thread, time};
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
-        stream.write_all(b"yo!").unwrap();
+
+        thread::spawn(move || {
+            loop {
+                stream.write_all(b"yo!").unwrap();
+    
+                let ten_millis = time::Duration::from_millis(1000);
+                let now = time::Instant::now();
+    
+                thread::sleep(ten_millis);
+            }
+        });
 
         // handle_connection(stream);
     }
